833a14d77f6aeb1b96c56e105878feb54fb84069
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-02-07 11:38:15 +0200

    CH1: Match - Vlidatelab1. Solved

---
+++ validateLab1Answer.js
@@ -0,0 +1,10 @@
+var pipeline = [
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Japanese"] },
+    },
+  },
+];
