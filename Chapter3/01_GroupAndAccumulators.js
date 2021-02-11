d57f2c6efc4063efa1e729e578208c27ac4f1e07
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2021-02-11 10:49:49 +0200

    CH3: Group and Accumulators. Solved.

---
+++ Chapter3/01_GroupAndAccumulators.js
@@ -0,0 +1,22 @@
+db.movies.aggregate([
+    {
+      $match: {
+        awards: { $exists: true, $ne: "", $regex: /Won\s[0-9]+\sOscar/i },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        highest_rating: { $max: "$imdb.rating" },
+        lowest_rating: { $min: "$imdb.rating" },
+        average_rating: { $avg: "$imdb.rating" },
+        average_deviation: { $stdDevSamp: "$imdb.rating" },
+      },
+    },
+    {
+      $set: {
+        average_rating: { $trunc: ["$average_rating", 4] },
+        average_deviation: { $trunc: ["$average_deviation", 4] },
+      },
+    },
+  ]).pretty()
